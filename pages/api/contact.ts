18c78cb4f2ca6b7c76efe7ff53a627279041378d
bwillem <bguenther3@gmail.com>
2023-03-15 10:37:27 -0700

    mailer endpoint

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,13 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import mailjet from 'node-mailjet'
+
+const mj = mailjet.apiConnect(
+  process.env.MJ_API_KEY_PUBLIC as string,
+  process.env.MJ_API_KEY_PRIVATE as string,
+  {},
+)
 
 type Data = {
-  name: string
+  data: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   console.log('api/contact', req.body)
-  res.status(200).json({ name: 'John Doe' })
+  p
+  try {
+    const request = await mj
+      .post('send', {})
+      .request({
+        Messages: [{
+          From: {},
+          To: [{}],
+          Subject: 'New contact form submission',
+          TextPart: req.body.message,
+        }],
+      })
+  } catch (e) {
+    console.error(e)
+    return res.status(500).json({ data: 'Error sending mail' })
+  }
+
+  return res.status(201).end()
 }
