d59cb29141a55d484ec681eab709ed006b2a4363
bwillem <bguenther3@gmail.com>
2023-03-28 22:32:50 -0700

    fix deploy

--- components/Video.tsx
+++ components/Video.tsx
@@ -2,7 +2,14 @@
 function Video() {
     return (
         <div className='relative max-w-full bg-primary-dark pt-[56.25%]'>
-            <iframe className='absolute top-[0px] left-[0px] w-full h-full' src="https://player.vimeo.com/video/806498185?h=d0c1767844" width="100%" height="auto" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <iframe 
+                className='absolute top-[0px] left-[0px] w-full h-full'
+                src="https://player.vimeo.com/video/806498185?h=d0c1767844"
+                width="100%"
+                height="auto"
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen/>
         </div>)
 }
 
