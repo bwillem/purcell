2b656481907f04fdf8b4011fbe34e30a5e871497
bwillem <bguenther3@gmail.com>
2023-04-25 20:17:41 -0700

    add gtag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import App from '@/components/App'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,16 @@ export default function Home() {
   return (
     <>
       <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BFPZP8FQ7V" />
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BFPZP8FQ7V');
+          `}
+        </Script>
         <title>Purcell Business Centre</title>
         <meta name="description" content="Purcell Business Centre | Nelson, BC" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
